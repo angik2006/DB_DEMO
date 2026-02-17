1ec860f519135d2d0ad0d943342c4d205ede71b5
Anzhelika Volosova <angik2006@gmail.com>
2026-02-17 13:35:18 +0000

    correction the data

--- models/station_dimention.sql
+++ models/station_dimention.sql
@@ -1,15 +1,19 @@
-with BIKE as (
+WITH BIKE as (
 
-    select 
-    distinct
-    start_statio_id as start_station_id,
-    start_station_name,
-    start_lat,
-    start_lng
-    
+select
+distinct
+START_STATIO_ID AS station_id,
+start_station_name as station_name,
+START_LAT as station_lat,
+START_LNG as start_station_lng
 
-    from {{ source('demo', 'bike') }}
-    where ride_id<> 'ride_id'
+from {{ ref('stg_bike') }}
+
+where RIDE_ID != '"bikeid"'
+
 
 )
-select * from BIKE
+
+select
+*
+from BIKE
